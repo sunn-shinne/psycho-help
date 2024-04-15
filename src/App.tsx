fb8f2ab83d86630872b34c65e8096bbb3be801ec
Ekaterina Brekhova <brekhova.katya@gmail.com>
2024-04-15 12:31:55 +0700

    Роутинг и хедер

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CSSProperties } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
+import HomePage from './pages/home-page/home-page';
+import Header from './components/header/header';
+
+const styles: { [key in string]: CSSProperties } = {
+  layout: {
+    overflow: 'hidden',
+    minHeight: '100vh',
+    width: '100vw',
+  },
+  contentWrapper: {
+    maxWidth: 1440,
+    margin: 'auto',
+    width: '100%',
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={styles.layout}>
+      <Header />
+      <Layout.Content>
+        <div style={styles.contentWrapper}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </Layout.Content>
+    </Layout>
   );
 }
 
---
+++ src/components/header/header.tsx
@@ -0,0 +1,32 @@
+import { CSSProperties } from 'react';
+import { Flex, Layout } from 'antd';
+import { ReactComponent as Logo } from '../../assets/images/logo.svg';
+
+const styles: { [key in string]: CSSProperties } = {
+  header: {
+    height: 100,
+    width: '100%',
+    margin: 'auto',
+    paddingTop: 24,
+    paddingBottom: 24,
+    backgroundColor: '#5E8BF4',
+    paddingInline: 0,
+  },
+  contentWrapper: {
+    maxWidth: 1440,
+    margin: 'auto',
+    width: '100%',
+  },
+};
+
+const Header = () => {
+  return (
+    <Layout.Header style={styles.header}>
+      <Flex style={styles.contentWrapper} justify="space-between">
+        <Logo />
+      </Flex>
+    </Layout.Header>
+  );
+};
+
+export default Header;
