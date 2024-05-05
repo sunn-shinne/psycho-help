18b1f0d7099a46209bb26fe47ad0bd0090bfe975
milkbarsik <mihail19092005@gmail.com>
2024-05-05 20:30:12 +0300

    Создал так же записаться на консультацию и график работы

---
+++ src/pages/home-page/can-sign-in-block/can-sign-in-block.tsx
@@ -0,0 +1,64 @@
+import { CSSProperties } from 'react';
+import HumanImage from '../../../assets/images/can-sign-in/human.svg';
+import PcImage from '../../../assets/images/can-sign-in/pc.svg';
+
+	const styles: { [key in string]: CSSProperties } = {
+	wrapper: {
+		display: "flex",
+		flexDirection: "column",
+		maxWidth: "100%",
+		justifyContent: "center"
+	},
+	textBlock: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "space-between",
+	},
+	mainInfo: {
+		display: "flex",
+		justifyContent: "space-between",
+		width: "100%",
+	},
+	p: {
+		marginLeft: "calc(3vh + 3vw)"
+	},
+	image: {
+		marginLeft: "calc(3vh + 3vw)"
+	},
+};
+
+const CanSignInBlock = () => {
+	return (
+		<div>
+			<div style = {styles.wrapper}>
+				<div style={styles.textBlock}>
+					<h3><span style={{marginRight: "calc((1vh + 1vw) / 2)"}}>—</span>через форму вконтакте</h3>
+					<div style={styles.mainInfo}>
+						<p style={styles.p}>Войдите в группу СПП в ВК — vk.com/spp_polytech <br />
+						Внимание! Обязательно дождитесь звонка специалиста и подтверждения записи на консультацию.</p>
+						<img style={styles.image} src = {HumanImage} alt="beautifull pictures"></img>
+					</div>
+				</div>
+				<div style={styles.textBlock}>
+					<h3><span style={{marginRight: "calc((1vh + 1vw) / 2)"}}>—</span>по электроноой почте</h3>
+					<div style={styles.mainInfo}>
+						<p style={styles.p}>Чтобы отправить письмо психологу, укажите Ф.И.О., группу (место работы) и ваш контактный телефон, по которому, в случае необходимости, можно с вами связаться. <br />
+							Для получения компетентного ответа психолога, лаконично, но информативно, опишите свою проблему (что беспокоит вас в данный момент с учетом «контекста ситуации»). <br />
+							Сформулируйте вопрос (запрос), который хотите задать психологу. <br />
+							Письмо рассматривается в течение 2-4 дней. <br />
+							Будьте внимательны! Ответ отправляется на указанную вами почту.</p>
+							<img style={styles.image} src = {PcImage} alt="beautifull pictures"></img>
+					</div>
+				</div>
+				<div style={styles.textBlock}>
+					<h3><span style={{marginRight: "calc((1vh + 1vw) / 2)"}}>—</span>по телефону: +7 (495) 223-05-23.</h3>
+					<div>
+						<p style={styles.p}></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default CanSignInBlock
---
+++ src/pages/home-page/chart-block/chart-block.tsx
@@ -0,0 +1,88 @@
+import { CSSProperties } from 'react';
+import ClockImage from '../../../assets/images/chart/clock.svg';
+
+const styles: { [key in string]: CSSProperties } = {
+	wrapper: {
+		position: "relative",
+		zIndex: 0,
+	},
+	wrapperBefore: {
+		content: "",
+		position: "absolute",
+		width: "200vw", 
+		height: "103%",
+		background: "rgb(217, 229, 255)",
+		zIndex: -1,
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: "-100vw",
+	},
+	textBlock: {
+		display: "flex",
+		alignItems: "center",
+	},
+	schedule: {
+		border: "1px black solid",
+		borderRadius: "10px",
+		padding: "calc((1vh + 1vw) / 2)",
+	},
+}
+
+const ChartBlock = () => {
+	return (
+		<div>
+			<div className="wrapper" style={styles.wrapper}>
+				<span style={styles.wrapperBefore}/>
+				<div style={{display: "flex", justifyContent: "space-between",}}>
+					<div style={styles.textBlock}>
+						<h3 style={{marginRight: "calc((1vh + 1vw) / 2)"}}>—</h3>
+						<p style={styles.schedule}>
+							<h3 style={{marginTop: 0}}>Расписание:</h3>
+							понедельник - четверг 9:30-18:30 <br />
+							пятница 9:30-17:15 <br />
+							перерыв на обед 13:00-13:45
+						</p>
+					</div>
+					<img src={ClockImage} alt="beatifull picture" />
+				</div>
+				<h3>
+					<span style={{marginRight: "calc((1vh + 1vw) / 2)", visibility: "hidden", pointerEvents: "none"}}>—</span>
+					В выходные и праздничные дни, ночью, за срочной психологической помощью можно 
+					обратиться по следующим телефонам:
+				</h3>
+				<div style={styles.textBlock}>
+					<h3 style={{marginRight: "calc((1vh + 1vw) / 2)"}}>—</h3>
+					<p>
+						Горячая линия Московской службы психологической помощи населению - 8 (495) 051 
+						(с городского телефона) и 051 (с мобильного телефона) <br />
+						<strong>Режим работы: круглосуточно</strong>
+					</p>
+				</div>
+				<div style={styles.textBlock}>
+					<h3 style={{marginRight: "calc((1vh + 1vw) / 2)"}}>—</h3>
+					<p>
+						Горячая линия по оказанию психологической помощи студенческой молодежи организованной Минобрнауки России - 8 (800) 222-55-71 <br />
+						<strong>Режим работы: круглосуточно</strong>
+					</p>
+				</div>
+				<div style={styles.textBlock}>
+					<h3 style={{marginRight: "calc((1vh + 1vw) / 2)"}}>—</h3>
+					<p>
+						Телефон доверия для детей, подростков и их родителей - 8 (800) 2000-122 <br />
+						<strong>Режим работы: круглосуточно</strong>
+					</p>
+				</div>
+				<div style={styles.textBlock}>
+					<h3 style={{marginRight: "calc((1vh + 1vw) / 2)"}}>—</h3>
+					<p>
+						Горячая линия центра экстренной психологической помощи МЧС России - 8 (495) 989-50-50 (бесплатно по Москве) <br />
+						<strong>Режим работы: круглосуточно</strong>
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ChartBlock
