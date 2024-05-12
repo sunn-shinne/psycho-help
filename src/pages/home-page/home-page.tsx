5e6f4c1f42fc88c07c2a8fa64699216f24621bd3
Ekaterina Brekhova <68824725+sunn-shinne@users.noreply.github.com>
2024-05-12 16:45:02 +0700

    Способы записи на консультацию и график работы
    Co-authored-by: milkbarsik <mihail19092005@gmail.com>

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -3,6 +3,8 @@ import { Typography } from 'antd';
 import GreetingBlock from './greeting-block/greeting-block';
 import ReasonsBlock from './reasons-block/reasons-block';
 import FeaturesBlock from './features-block/features-block';
+import CanSignInBlock from './can-sign-in-block/can-sign-in-block';
+import ChartBlock from './chart-block/chart-block';
 
 const styles: { [key in string]: CSSProperties } = {
   block: {
@@ -34,6 +36,18 @@ const HomePage = () => {
           Особенности работы службы:
         </Typography.Title>
         <FeaturesBlock />
+      </div>
+			<div style={styles.block}>
+        <Typography.Title level={3} style={styles.title}>
+          Так же записатья на консультацию можно:
+        </Typography.Title>
+        <CanSignInBlock />
+      </div>
+			<div style={styles.block}>
+        <Typography.Title level={3} style={styles.title}>
+          График работы:
+        </Typography.Title>
+        <ChartBlock />
       </div>
     </div>
   );
