939f128405e5a124af1e7784b344b31487d41fce
Ekaterina Brekhova <brekhova.katya@gmail.com>
2024-05-12 13:44:39 +0300

    Верстка формы отправки заявки на консультацию

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -7,6 +7,7 @@ import CanSignInBlock from './can-sign-in-block/can-sign-in-block';
 import ChartBlock from './chart-block/chart-block';
 import QuestionList from './question-block/question-list';
 import Footer from './footer-block/footer-block';
+import SignInBlock from './sign-in-block/sign-in-block';
 
 const styles: { [key in string]: CSSProperties } = {
   block: {
@@ -20,14 +21,37 @@ const styles: { [key in string]: CSSProperties } = {
 };
 
 const HomePage = () => {
-
-	const questions = [
-		{id:1, ask: 'В каком режиме проходят консультации?', answer: 'Консультации проводятся очно в кабинетах службы и дистанционно с помощью: мобильной связи, мессенджеров, зума, скайпа, электронной почты. Они бесплатны и конфиденциальны.'},
-		{id:2, ask: 'Сколько времени длится консультация?', answer: 'Длительность заранее запланированной консультации 50 мин. - 1 час.'},
-		{id:3, ask: 'Как отменить или перенести консультацию?', answer: 'При необходимости переноса или отмены консультации, обучающийся обязан предупредить специалиста службы психологической помощи минимум за 1 сутки до запланированной встречи;'},
-		{id:4, ask: 'Можно ли опоздать на консультацию?', answer: 'Можно, но вы должны заблаговременно предупредить сотрудника СПП об опоздании. Если вы опоздали на консультацию на 15 мин. и не предупредили об этом, консультация отменяется.'},
-		{id:5, ask: 'Можно ли получить консультацию вне очереди?', answer: 'Если Вы находитесь в остром, кризисном состоянии, то возможна внеочередная срочная консультация в день обращения продолжительностью от 15 до 30 минут.'}
-	];
+  const questions = [
+    {
+      id: 1,
+      ask: 'В каком режиме проходят консультации?',
+      answer:
+        'Консультации проводятся очно в кабинетах службы и дистанционно с помощью: мобильной связи, мессенджеров, зума, скайпа, электронной почты. Они бесплатны и конфиденциальны.',
+    },
+    {
+      id: 2,
+      ask: 'Сколько времени длится консультация?',
+      answer: 'Длительность заранее запланированной консультации 50 мин. - 1 час.',
+    },
+    {
+      id: 3,
+      ask: 'Как отменить или перенести консультацию?',
+      answer:
+        'При необходимости переноса или отмены консультации, обучающийся обязан предупредить специалиста службы психологической помощи минимум за 1 сутки до запланированной встречи;',
+    },
+    {
+      id: 4,
+      ask: 'Можно ли опоздать на консультацию?',
+      answer:
+        'Можно, но вы должны заблаговременно предупредить сотрудника СПП об опоздании. Если вы опоздали на консультацию на 15 мин. и не предупредили об этом, консультация отменяется.',
+    },
+    {
+      id: 5,
+      ask: 'Можно ли получить консультацию вне очереди?',
+      answer:
+        'Если Вы находитесь в остром, кризисном состоянии, то возможна внеочередная срочная консультация в день обращения продолжительностью от 15 до 30 минут.',
+    },
+  ];
 
   return (
     <div>
@@ -48,26 +72,32 @@ const HomePage = () => {
         </Typography.Title>
         <FeaturesBlock />
       </div>
-			<div style={styles.block}>
+      <div style={styles.block}>
+        <Typography.Title level={3} style={styles.title}>
+          Записаться прямо сейчас:
+        </Typography.Title>
+        <SignInBlock />
+      </div>
+      <div style={styles.block}>
         <Typography.Title level={3} style={styles.title}>
           Так же записатья на консультацию можно:
         </Typography.Title>
         <CanSignInBlock />
       </div>
-			<div style={styles.block}>
+      <div style={styles.block}>
         <Typography.Title level={3} style={styles.title}>
           График работы:
         </Typography.Title>
         <ChartBlock />
       </div>
-			<div style={styles.block}>
+      <div style={styles.block}>
         <Typography.Title level={3} style={styles.title}>
           Часто задаваемые вопросы:
         </Typography.Title>
-        <QuestionList questions = {questions}/>
+        <QuestionList questions={questions} />
       </div>
-			<div style={{...styles.block, paddingBottom: 0}}>
-        <Footer/>
+      <div style={{ ...styles.block, paddingBottom: 0 }}>
+        <Footer />
       </div>
     </div>
   );
