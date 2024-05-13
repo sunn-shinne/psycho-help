ac0b65f4ce9a54c0ba87943d45e08549ea12e8bd
Ekaterina Brekhova <68824725+sunn-shinne@users.noreply.github.com>
2024-05-13 12:00:37 +0300

    Список докторов

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
