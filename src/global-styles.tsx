faa3ca184fd5b894101cf72d19dda7861101572e
Artem <113698975+zemscript@users.noreply.github.com>
2024-11-04 18:04:12 +0300

    Модальное окно регистрации

--- src/global-styles.tsx
+++ src/global-styles.tsx
@@ -1,9 +1,21 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
     :root {
 
     }
+`;
+
+export const ErrorText = styled.span`
+  color: red;
+  font-size: 12px;
+`;
+
+export const Form = styled.form`
+  label {
+    margin-bottom: 10px;
+    display: block;
+  }
 `;
 
 export default GlobalStyles;
