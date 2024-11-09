3d9a67c6d76399246ded9cd1330d2015358c4124
Reuzer <SergeyReunin1@gmail.com>
2024-11-09 21:14:55 +0300

    fixingsFromReview

---
+++ src/pages/home-page/components/question-block/question.tsx
@@ -0,0 +1,62 @@
+import { useState, CSSProperties } from 'react';
+import buttonImage from '../../../../assets/images/main/question/button.svg';
+import ActivebuttonImage from '../../../../assets/images/main/question/active-button.svg';
+
+const Question = ({ id, ask, answer }: { id: Number; ask: string; answer: string }) => {
+  const [isActive, setActive] = useState(false);
+
+  function OnClick() {
+    setActive((isActive) => !isActive);
+  }
+
+  const styles: { [key in string]: CSSProperties } = {
+    wrapper: {
+      width: '100%',
+      height: 'auto',
+      marginTop: `${id === 1 ? '0' : 'calc((1vh + 1vw) / 4)'}`,
+      padding: '0 calc((1vh + 1vw) / 2)',
+      cursor: 'pointer',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    ask: {
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      color: isActive ? '#5E8BF4' : 'black',
+    },
+    button: {
+      borderRadius: '50%',
+      height: 34,
+      width: 34,
+      backgroundImage: `url(${isActive ? ActivebuttonImage : buttonImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      marginRight: 'calc((1vh + 1vw) / 2)',
+    },
+
+    answer: {
+      marginLeft: 'calc((1vh + 1vw) / 2 + 34px)',
+    },
+    p: {
+      wordBreak: 'break-word',
+      flexGrow: 1,
+      width: '100%',
+      whiteSpace: 'pre-wrap',
+    },
+  };
+
+  return (
+    <div style={styles.wrapper} onClick={OnClick}>
+      <div style={styles.ask}>
+        <div style={styles.button}></div>
+        <p style={styles.p}>{ask}</p>
+      </div>
+      {isActive && <p style={styles.answer}>{answer}</p>}
+    </div>
+  );
+};
+
+export default Question;
---
+++ src/pages/home-page/components/question-block/questions-list.tsx
@@ -0,0 +1,30 @@
+import Question from './question';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  border-radius: 0px;
+  overflow: hidden;
+  height: auto;
+  background: transparent;
+`;
+
+const QuestionsList = ({
+  questions,
+}: {
+  questions: Array<{ id: number; ask: string; answer: string }>;
+}) => {
+  return (
+    <Wrapper>
+      {questions.map((questionObj) => (
+        <Question
+          ask={questionObj.ask}
+          id={questionObj.id}
+          answer={questionObj.answer}
+          key={questionObj.id}
+        />
+      ))}
+    </Wrapper>
+  );
+};
+
+export default QuestionsList;
